6fc8c5827f1fee84d901e004d16ebfc23d6154a5
bumsly <65000254+bumsly@users.noreply.github.com>
2023-10-12 18:48:23 +0900

    beginning

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
+function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />}>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
